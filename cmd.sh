05ff48ed2dac2a5c877b66033f2a59cdc4c85fa8
Pierre Pacchioni <ppacch@yahoo.com>
2017-01-31 09:34:22 +0000

    cmd

--- cmd.sh
+++ cmd.sh
@@ -2,12 +2,12 @@
 set -e 
 
 if [ "$ENV" = 'DEV' ]; then
-    echo "Running Development server"
+    echo "Running Development Server"
     exec python "identidock.py"
 elif [ "$ENV" = 'UNIT' ]; then
-    echo "Running unit tests"
+    echo "Running Unit Tests"
     exec python "tests.py"
 else
-    echo "Running production server"
+    echo "Running Production Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
 fi
